1ca425bb9bbab6404dfd1d9a3c0d8e349046a886
MHDN55 <135222624+MHDN55@users.noreply.github.com>
2024-02-11 17:15:29 +0300

    second change

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 // ignore_for_file: avoid_print
+import 'dart:convert';
 import 'package:e_commerce_app/firebase_options.dart';
 import 'package:e_commerce_app/core/routes/app_route_config.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'injection_injectable_package.dart' as di;
+import 'package:http/http.dart' as http;
 
 void main() async {
   di.configureDependencies();
@@ -36,7 +39,13 @@ class _MyAppState extends State<MyApp> {
         }
       },
     );
+    myGetToken();
     super.initState();
+  }
+
+  myGetToken() async {
+    String? token = await FirebaseMessaging.instance.getToken();
+    print("======================================================$token");
   }
 
   @override
@@ -51,5 +60,37 @@ class _MyAppState extends State<MyApp> {
         debugShowCheckedModeBanner: false,
       ),
     );
+  }
+}
+
+sendNotification(String titleMessage, String bodyMessage) async {
+  var headersList = {
+    'Accept': '*/*',
+    'Content-Type': 'application/json',
+    'Authorization':
+        'key=AAAAnongGGA:APA91bE75rEMIL50myYHJ5wJQLhRuIgEQ1v82Ja5qQwWmVFRZ7VS1f_3bMxqz6GcMojY8DeZg5bL5mVT1WbNSj7L-5nZeotycjjW09ZJBYFlujEMwKRlb0qNI3CDo3o-0ovMb5JT0L-M'
+  };
+  var url = Uri.parse('https://fcm.googleapis.com/fcm/send');
+
+  var body = {
+    "to":
+        "ftnaYjkkRVGTOHcwXKRRU3:APA91bHgFJokTpCtkY376fbYibWRRzfVBI4IER8Z5-pvWuTqeN7YuNWpFVcCblPieK0Px8vp7QUXbWG4c2EeUBRXKsGYkbUwpnXhp6av_VuBwYGwvz7I-V_2tNeb8nmb07k6RCFCExuV",
+    "notification": {
+      "title": titleMessage,
+      "body": bodyMessage,
+    }
+  };
+
+  var req = http.Request('POST', url);
+  req.headers.addAll(headersList);
+  req.body = json.encode(body);
+
+  var res = await req.send();
+  final resBody = await res.stream.bytesToString();
+
+  if (res.statusCode >= 200 && res.statusCode < 300) {
+    print(resBody);
+  } else {
+    print(res.reasonPhrase);
   }
 }
